a2a59c1c9390bc3bab0d947e3a0ce0a52a088954
sv-yarakaev <yarakaeff@gmail.com>
2020-04-17 21:46:17 +0300

    simple and with danger func

--- doubles/double.c
+++ doubles/double.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <ctype.h>
+#include <string.h>
 
-int lineum;
 
-void doubleword(char*, FILE *);
+int linenum;
 
+void doubleword(char *, FILE *);
+int getword(FILE *, char *, int);
 
 
 int main(int argc, char* argv[]) {
@@ -29,15 +31,33 @@ int main(int argc, char* argv[]) {
 void doubleword(char *name, FILE *fp) {
     char prev[128], word[128];
 
-    lineum = 1;
+    linenum = 1;
     prev[0] ='\0';
     while (getword(fp, word, sizeof (word))) {
-        if (isalpha(word[0] && strcmp(prev, word)==0
-            <word_duplicate>
+        if (isalpha(word[0] && strcmp(prev, word)==0)) {
+            if (name) printf("%s:", name);
+            printf("%d: %s\n", linenum, word);
+        }
          strcpy(prev, word);
     }
 }
 
-
+int getword(FILE *fp, char *buf, int size) {
+    int c;
+    c = getc(fp);
+    for ( ; c != EOF && isspace(c); c = getc(fp))
+        if (c == '\n') linenum++;
+    {
+    int i = 0;
+    for ( ; c != EOF && !isspace(c); c = getc(fp))
+        if (i < size - 1)
+            buf[i++] = tolower(c);
+        if (i < size)
+            buf[i] = '\0';
+    }
+    if ( c != EOF)
+        ungetc(c, fp);
+    return buf[0] != '\0';
+}
 
 
