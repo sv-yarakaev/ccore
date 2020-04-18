a6a8f3229d764a0dc719538208deb4aa610c2d2c
sv-yarakaev <yarakaeff@gmail.com>
2020-04-18 23:15:33 +0300

    Improve interface stack

--- adt/stack.h
+++ adt/stack.h
@@ -1,12 +1,17 @@
 #ifndef STACK_H
 #define STACK_H
 
-typedef struct Stack_T *Stack_T;
+/*
+ * typedef struct Stack_T *Stack_T;
+*/
+// New interface
+#define T Stack_T
+typedef struct T *T;
 
 extern Stack_T Stack_new(void);
-extern int Stack_empty(Stack_T stk);
-extern void Stack_push(Stack_T stk, void *x);
-extern void *Stack_pop (Stack_T stk);
-extern void Stack_free (Stack_T *stk);
+extern int Stack_empty(T stk);
+extern void Stack_push(T stk, void *x);
+extern void *Stack_pop (T stk);
+extern void Stack_free (T *stk);
 
 #endif
