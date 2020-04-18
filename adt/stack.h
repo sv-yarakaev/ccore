7312e7ab8ca0362daf787de994d9beee787cc96c
sv-yarakaev <yarakaeff@gmail.com>
2020-04-18 23:12:43 +0300

    Create interface fo ADT stack

---
+++ adt/stack.h
@@ -0,0 +1,12 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct Stack_T *Stack_T;
+
+extern Stack_T Stack_new(void);
+extern int Stack_empty(Stack_T stk);
+extern void Stack_push(Stack_T stk, void *x);
+extern void *Stack_pop (Stack_T stk);
+extern void Stack_free (Stack_T *stk);
+
+#endif
