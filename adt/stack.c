fe3a672794785b19da4572efd7a694da32d6b162
sv-yarakaev <yarakaeff@gmail.com>
2020-04-19 13:39:42 +0300

    Add implementation for stack

--- adt/stack.c
+++ adt/stack.c
@@ -26,4 +26,37 @@ int Stack_empty(T stk) {
     return stk->count == 0;
 }
 
+void Stack_push(T stk, void *x) {
+    struct elem *t;
+    
+    assert(stk);
+    NEW(t);
+    t->x =x;
+    t->link = stk->head;
+    stk->head = t;
+    stk->count++;
+}
+
+void *Stack_pop(T stk) {
+    void *x;
+    struct elem *t;
+    assert(stk);
+    assert(stk->count > 0);
+    t = stk->head;
+    stk->head = t->link;
+    stk->count--;
+    x = t->x;
+    FREE(t);
+    return x;
+}
+
+void Stack_free(T *stk) {
+    struct elem *t, *u;
+    assert(stk && *stk);
+    for (t = (*stk)->head; t; t = u) {
+        u = t->link;
+        FREE(t);
+    }
+    FREE(*stk);
+}
 
