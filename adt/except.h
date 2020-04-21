38e921c04d470f06c6f2185e3a2bb1b31dadec25
sv-yarakaev <yarakaeff@gmail.com>
2020-04-21 22:33:20 +0300

    Add definition function and macros Raise

--- adt/except.h
+++ adt/except.h
@@ -7,6 +7,13 @@ typedef struct T {
     const char *reason;
 } T;
 
+#define RAISE(e) Except_raise(&(e), __FILE__, __LINE__)
+
+void Except_raise(const T *e, const char *file, int line);
+
+
+
+
 #undef T
 
 
