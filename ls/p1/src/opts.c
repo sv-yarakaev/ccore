f410b4cf7d01f76d09416b78a8428c1bb1817eb5
sv-yarakaev <yarakaeff@gmail.com>
2020-04-22 22:17:10 +0300

    add full arguments to getopt_long

--- ls/p1/src/opts.c
+++ ls/p1/src/opts.c
@@ -2,6 +2,9 @@
 #include <getopt.h> /* GLIBC */
 #include <unistd.h>
 
+extern char *optarg;
+extern int optind, opterr, optopt;
+
 int getopt_long(int argc, char *const argv[], const char *optstring,
         const struct option *longopts, int *longindex);
 int getopt_long_only(int argc, char *const argv[],
@@ -13,31 +16,57 @@ int getopt_long_only(int argc, char *const argv[],
  * then me call "table of long options"
  * */
 int do_all, do_help, do_verbose;
-char *myfile;
+char *myfile, *user;
 
 struct option longopts[] = {
     {"all", no_argument, &do_all, 1 },
     {"file", required_argument, NULL, 'f'},
     {"help", no_argument, &do_help, 1 },
     {"verbose", no_argument, &do_verbose, 1},
+    {"user", optional_argument, NULL, 'u'},
     {0, 0, 0, 0 }
 };
 
 int main(int argc, char *argv[]) {
     int oc;
-    char c;
+    int c;
     char *b_opt_arg;
-    while ((oc = getopt_long(argc, argv, ":f:", longopts, NULL)) != -1 ) {
+    while ((oc = getopt_long(argc, argv, ":ahvf:u::W;", longopts, NULL)) != -1 ) {
         switch (oc) {
-        case 'f':
-            myfile = optarg;
-            break;
+            case 'a':
+                do_all = 1;
+                break;
+
+            case 'f':
+                myfile = optarg;
+                break;
+            case 'h':
+                do_help = 1;
+                break;
+            case 'u':
+                if (optarg != NULL)
+                    user = optarg;
+                else
+                    user = "root";
+                break;
+            case 'v':
+                do_verbose = 1;
+                break;
         case 0:
+            break;
+        case ':' :
+            fprintf(stderr, "%s: option `-%c' required an argument\n",
+                    argv[0], optopt);
+            break;
+        case '?':
+        default:
+            fprintf(stderr, "%s: option `-%c' is invalid: ignored\n",
+                    argv[0], optopt);
             break;
         }
     }
     
-    }
+    
 
     return 0;
 }
