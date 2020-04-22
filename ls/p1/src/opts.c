7820a5081737b05be2959b2576f1c9cf99129249
sv-yarakaev <yarakaeff@gmail.com>
2020-04-22 22:01:26 +0300

    change to getop_long

--- ls/p1/src/opts.c
+++ ls/p1/src/opts.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
+#include <getopt.h> /* GLIBC */
 #include <unistd.h>
 
-int getopt(int argc, char *const argv[], const char *optstring);
-extern char *optarg;
-extern int optind, opterr, optopt;
+int getopt_long(int argc, char *const argv[], const char *optstring,
+        const struct option *longopts, int *longindex);
+int getopt_long_only(int argc, char *const argv[],
+        const char *optstring,
+        const struct option *longopts, int *longindex);
+
+/* 
+ * Fist three arguments is the same  as  in getopt(). Next option is pointer to array struct option,
+ * then me call "table of long options"
+ * */
+int do_all, do_help, do_verbose;
+char *myfile;
+
+struct option longopts[] = {
+    {"all", no_argument, &do_all, 1 },
+    {"file", required_argument, NULL, 'f'},
+    {"help", no_argument, &do_help, 1 },
+    {"verbose", no_argument, &do_verbose, 1},
+    {0, 0, 0, 0 }
+};
 
 int main(int argc, char *argv[]) {
     int oc;
+    char c;
     char *b_opt_arg;
-    while ((oc = getopt(argc, argv, "ab:")) != -1 ) {
-	 switch (oc){
-            case 'a':
-                printf("Parsing a optins\n");
-                break;
-            case 'b':
-                b_opt_arg = optarg;
-                printf("Parsing -b with arg %s\n", b_opt_arg);
-                break;
-            case ':':
-                fprintf(stderr, "%s: option -%c requires an argument\n", argv[0], optopt);
-                break;
-            case '?' :
-            default:
-                fprintf(stderr, "%s: option `-%c is invalid: ignored\n", argv[0], optopt); 
-                return -1;
+    while ((oc = getopt_long(argc, argv, ":f:", longopts, NULL)) != -1 ) {
+        switch (oc) {
+        case 'f':
+            myfile = optarg;
+            break;
+        case 0:
+            break;
         }
     }
+    
+    }
+
     return 0;
 }
